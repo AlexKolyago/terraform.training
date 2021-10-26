2f0213adcc84e5d4b8030ab2b01485d794bc6f97
Aliaksei Kaliaha <aliaksei_kaliaha@epam.com>
2021-10-26 16:53:06 +0300

    Initial comm

---
+++ tf1/main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+
+  region = "eu-central-1"
+
+}
+
+resource "aws_instance" "example" {
+
+  ami           = "ami-05f7491af5eef733a"
+
+  instance_type = "t2.micro"
+
+  tags = {
+
+    Name = "terraform-example"
+
+  }
+
+}
