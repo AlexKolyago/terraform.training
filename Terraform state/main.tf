e3c6d00bdfd2d374e863bea9bb9aba7c02ba0f3b
AlexKolyago <aleksey.kolyago.97@mail.ru>
2021-11-04 19:54:12 +0300

    S3+DynamoDB+ .tfstate inside

--- Terraform state/main.tf
+++ Terraform state/main.tf
@@ -2,14 +2,14 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-resource "aws_s3_bucket" "terraform-alex-kolyago-state" {
-  bucket = "terraform-state"
+resource "aws_s3_bucket" "terraform-alex-kolyago-training-state" {
+  bucket = "terraform-alex-kolyago-training-state"
 
   # force_destroy = true
 
-  lifecycle {
-    prevent_destroy = true
-  }
+  #lifecycle {
+  #  prevent_destroy = true
+  #}
 
   versioning {
     enabled = true
@@ -25,12 +25,32 @@ resource "aws_s3_bucket" "terraform-alex-kolyago-state" {
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
-  name = "terraform-alex-kolyago-state"
+  name         = "terraform-alex-kolyago-state"
   billing_mode = "PAY_PER_REQUEST"
-  hash_key = "LockID"
+  hash_key     = "LockID"
   
   attribute {
     name = "LockID"
     type = "S"
   }
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "terraform-alex-kolyago-training-state"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "terraform-alex-kolyago-state"
+    encrypt        = true
+  }
+}
+
+output "s3_bucket_arn" {
+  value       = aws_s3_bucket.terraform-alex-kolyago-training-state.arn
+  description = "The ARN of the S3 bucket"
+}
+
+output "dynamodb_table_name" {
+  value       = aws_dynamodb_table.terraform_locks.name
+  description = "The name of the DynamoDB table"
 }
--- WebServer/main.tf
+++ WebServer/main.tf
@@ -3,18 +3,22 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-00d5329b6464d26f5"
-  instance_type = "t2.micro"
+  ami                    = "ami-03a71cec707bfc3d7"
+  instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
 
-  user_data = <<-EOF
-    #!/bin/bash
-    echo "HI"> index.html
-    nogup busybox httpd -f -p 8080 &
-    EOF
+  user_data              = <<EOF
+#!/bin/bash
+yum -y update
+yum - y install httpd
+ip='curl http://169.254.169.254/latest/meta-data/local-ipv4'
+echo "<h2>WebServer with IP: $ip</h2> by Terraform!" > /var/www/html/index.html
+sudo service httpd start
+chkconfig httpd on
+EOF
 
   tags = {
-    Name = "terraform-example"
+    Name = "webServer-terraform-example"
   }
 }
 
@@ -23,9 +27,23 @@ resource "aws_security_group" "instance" {
   name = var.security_group_name
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = [ "0.0.0.0/0" ]
   }
 }
